cdfe64a1af12ea3e4923a6c48e371d8cf9a799df
souhila boukari <suhamimisou14@gmail.com>
2022-04-08 04:12:40 -0400

    adding get llanding page

--- server.js
+++ server.js
@@ -7,30 +7,36 @@ const notesData = require("./db/db.json");
 const app = express();
 
 const createNewNote = function (body) {
-  const newNote = body;
-  notesData.push(newNote);
+    const newNote = body;
+    notesData.push(newNote);
 
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify(notesData, null, 2)
-  );
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesData, null, 2)
+    );
 
-  return newNote;
+    return newNote;
 };
 
 const deleteNote = function (id) {
-    
+
     notesData.splice(id - 1, 1);
-  
+
     fs.writeFileSync(
-      path.join(__dirname, "./db/db.json"),
-      JSON.stringify(notesData, null, 2)
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesData, null, 2)
     );
-  
+
     return notesData;
-  };
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, "public")));
-  
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", function (req, res, next) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res, next) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
